ce5ddeeb8e090d9a26a19f3d5753745d72383fac
Tamires da Hora <thora@id.uff.br>
2022-08-21 13:41:16 -0300

    Movimentação basica do player e ajustes nos menus

---
+++ src/Scenes/Interação.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+signal interacao_ativada
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_Area2D_body_entered(body):
+	print('entrou')
+	emit_signal("interacao_ativada")
---
+++ src/Scripts/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var speed = 50
+var velocity = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	velocity = Vector2()
+	
+	if Input.is_action_pressed("cima"):
+		velocity.y -= speed
+	
+	if Input.is_action_pressed("baixo"):
+		velocity.y += speed
+	
+	if Input.is_action_pressed("esquerda"):
+		velocity.x -= speed
+	
+	if Input.is_action_pressed("direita"):
+		velocity.x += speed
+		
+	move_and_slide(velocity)
